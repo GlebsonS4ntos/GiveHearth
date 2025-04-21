5d8ab684a3f5f975becf8f0b2cf08659bf8255f8
Glebson Santos <glebsonsantos@outlook.pt>
2025-04-21 03:19:34 -0300

    feat: add model registration

---
+++ Models/Register.cs
@@ -0,0 +1,11 @@
+﻿namespace GiveHearth.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf {  get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
