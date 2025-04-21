a5703dfd02d381830f6bc67d549ae1bf0f618b90
Glebson Santos <glebsonsantos@outlook.pt>
2025-04-21 17:34:05 -0300

    feat: Register Controller

---
+++ Controllers/RegistersController.cs
@@ -0,0 +1,57 @@
+﻿using GiveHearth.Dtos;
+using GiveHearth.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GiveHearth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistersController : ControllerBase
+    {
+        private readonly IServiceRegister _serviceRegister;
+
+        public RegistersController(IServiceRegister serviceRegister)
+        {
+            _serviceRegister = serviceRegister;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegistersAsync()
+        {
+            return Ok(await _serviceRegister.GetAllAsync());
+        }
+
+        [HttpGet("cpf")]
+        public async Task<IActionResult> GetAllRegistersByCpfAsync([FromQuery] string cpf)
+        {
+            return Ok(await _serviceRegister.GetAllByCpfAsync(cpf));
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetRegisterByIdAsync([FromRoute] int id)
+        {
+            return Ok(await _serviceRegister.GetByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRegisterAsync([FromBody] RegisterDto dto)
+        {
+            var register = await _serviceRegister.CreateAsync(dto);
+            return Created($"register/{register.Id}", register);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateRegisterAsync([FromBody] RegisterDto dto, [FromRoute] int id)
+        {
+            await _serviceRegister.UpdateAsync(dto, id);
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteRegisterAsync([FromRoute] int id)
+        {
+            await _serviceRegister.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
