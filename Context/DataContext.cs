dc026fc9fcc8d31276d21ba9ef4ef0f0cd17f51c
Glebson Santos <glebsonsantos@outlook.pt>
2025-04-21 03:20:09 -0300

    feat: add bdcontext

---
+++ Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using GiveHearth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiveHearth.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Register> registrations {  get; set; }
+
+
+    }
+}
