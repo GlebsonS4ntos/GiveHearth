4c36815fad143b9bff6b2b2c9d1cca97abd8c935
Glebson Santos <glebsonsantos@outlook.pt>
2025-04-21 14:42:36 -0300

    feat: add register mapper

---
+++ Config/RegisterProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GiveHearth.Dtos;
+using GiveHearth.Models;
+
+namespace GiveHearth.Config
+{
+    public class RegisterProfile : Profile
+    {
+        public RegisterProfile() 
+        { 
+            CreateMap<Register, RegisterDto>().ReverseMap();
+        }
+    }
+}
