b6effe0dbc2a06fbbeb53d4416de992b623a7f5a
Glebson Santos <glebsonsantos@outlook.pt>
2025-04-21 14:16:09 -0300

    feat: Register Dto

---
+++ Dtos/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GiveHearth.Dtos
+{
+    public class RegisterDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
